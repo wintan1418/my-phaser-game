f6dcc76512c77f3c155609a4f7df2e5d6cc984b7
oluwadare <wintan1418@gmail.com>
2021-06-23 14:02:47 +0100

    add logic for  for preload functions and create

---
+++ src/base.js
@@ -0,0 +1,34 @@
+import Phaser from 'phaser';
+import config from './config/config';
+import Audio from './util/Audio';
+import GameScene from './scenes/GameScene';
+import BootScene from './scenes/BootScene';
+import PreloaderScene from './scenes/PreloaderScene';
+import TitleScene from './scenes/TitleScene';
+import OptionsScene from './scenes/OptionsScene';
+import CreditsScene from './scenes/CreditsScene';
+import GameOver from './scenes/GameOver';
+// import Starfield from './scenes/Starfield';
+import LeaderBoard from './scenes/LeaderBoard';
+
+export default class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    this.globals = {
+      score: 0,
+    };
+    const audio = new Audio();
+    this.globals = { audio, bgMusic: null };
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
+    this.scene.add('GameScene', GameScene);
+    this.scene.add('GameOver', GameOver);
+    this.scene.add('LeaderBoard', LeaderBoard);
+    // this.scene.add('Starfield', Starfield);
+  }
+}
+
+window.game = new Game();
