9ba1f10822ec51c81c72cb03d96f10ff39963f77
oluwadare <wintan1418@gmail.com>
2021-06-23 17:31:29 +0100

    add and complete titlescene setup

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -27,3 +27,23 @@ export default class TitleScene extends Phaser.Scene {
       'Options',
       'Options',
     );
+
+    this.creditsButton = new Button(
+      this,
+      config.width / 2,
+      config.height / 2 + 100,
+      'blueButton1',
+      'blueButton2',
+      'Credits',
+      'Credits',
+    );
+
+    this.audio = this.sys.game.globals.audio;
+    if (this.audio.musicOn === true && this.audio.bgMusicPlaying === false) {
+      this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
+      this.bgMusic.play();
+      this.audio.bgMusicPlaying = true;
+      this.sys.game.globals.bgMusic = this.bgMusic;
+    }
+  }
+}
