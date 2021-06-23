ba5b5df696cde4d59031546583a1d2d32c934b16
oluwadare <wintan1418@gmail.com>
2021-06-23 17:25:12 +0100

    add code for preloader graphics

---
+++ src/scenes/PreloaderScene.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser';
+
+export default class PreloaderScene extends Phaser.Scene {
+  constructor() {
+    super('Preloader');
+  }
+
+  preload() {
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
