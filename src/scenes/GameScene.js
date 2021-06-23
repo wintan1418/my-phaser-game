3e2e05d43bb1d74a15d4777f6534204d6daa67ae
oluwadare <wintan1418@gmail.com>
2021-06-23 17:18:00 +0100

    finish code for game scene

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -41,162 +41,177 @@ export default class GameScene extends Phaser.Scene {
       font: '24px Courier',
       fill: '#00ff00',
     });
-// Player Ship
-this.player = new Player(this, 400, 500, 'player').setScale(0.5);
-this.add.existing(this.player);
+    // Player Ship
+    this.player = new Player(this, 400, 500, 'player').setScale(0.5);
+    this.add.existing(this.player);
 
-// Sounds
-this.sfx = {
-  laser: this.sound.add('laser-sound'),
-  gameOver: this.sound.add('game-over-sound'),
-};
+    // Sounds
+    this.sfx = {
+      laser: this.sound.add('laser-sound'),
+      gameOver: this.sound.add('game-over-sound'),
+    };
 
-// KEYS
-this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-this.keyD = this.input.keyboard.addKey(
-  Phaser.Input.Keyboard.KeyCodes.RIGHT,
-);
-this.space = this.input.keyboard.addKey(
-  Phaser.Input.Keyboard.KeyCodes.SPACE,
-);
-
-// Groups
-this.enemies = this.add.group();
-this.enemyLasers = this.add.group();
-this.playerLasers = this.add.group();
-
-// Enemies
-this.time.addEvent({
-  delay: 800,
-  callback() {
-    let enemy = null;
-
-    if (Phaser.Math.Between(0, 10) >= 3) {
-      enemy = new EnemyOne(this, Phaser.Math.Between(0, config.width), 0);
-    } else {
-      enemy = new EnemyTwo(this, Phaser.Math.Between(0, config.width), 0);
-    }
-
-    if (enemy !== null) {
-      enemy.setScale(Phaser.Math.Between(3, 6) * 0.1);
-      this.enemies.add(enemy);
-    }
-  },
-  loop: true,
-  callbackScope: this,
-});
-}
-
-update() {
-if (!this.player.getData('dead')) {
-  this.player.update();
-  if (this.keyW.isDown) {
-    this.player.up();
-  } else if (this.keyS.isDown) {
-    this.player.down();
-  }
-  if (this.keyA.isDown) {
-    this.player.left();
-  } else if (this.keyD.isDown) {
-    this.player.right();
-  }
-  if (this.space.isDown) {
-    this.player.setData('shooting', true);
-  } else {
-    this.player.setData(
-      'shootTime',
-      this.player.getData('shotFrequency') - 1,
+    // KEYS
+    this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    this.keyD = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.RIGHT,
     );
-    this.player.setData('shooting', false);
+    this.space = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE,
+    );
+
+    // Groups
+    this.enemies = this.add.group();
+    this.enemyLasers = this.add.group();
+    this.playerLasers = this.add.group();
+
+    // Enemies
+    this.time.addEvent({
+      delay: 800,
+      callback() {
+        let enemy = null;
+
+        if (Phaser.Math.Between(0, 10) >= 3) {
+          enemy = new EnemyOne(this, Phaser.Math.Between(0, config.width), 0);
+        } else {
+          enemy = new EnemyTwo(this, Phaser.Math.Between(0, config.width), 0);
+        }
+
+        if (enemy !== null) {
+          enemy.setScale(Phaser.Math.Between(3, 6) * 0.1);
+          this.enemies.add(enemy);
+        }
+      },
+      loop: true,
+      callbackScope: this,
+    });
   }
-}
 
-// If crash then game over
-this.physics.add.overlap(
-  this.player,
-  this.enemies,
-  function crash(player, enemy) {
-    if (!player.getData('dead') && !enemy.getData('dead')) {
-      this.sfx.gameOver.play();
-      player.dead(false);
-      player.shot();
-      enemy.dead(true);
-      this.sys.game.globals.score = this.scoreTotal;
-      this.scoreTotal = 0;
-    }
-  },
-  null,
-  this,
-);
-
-// If shot enemy then enemy dies
-this.physics.add.collider(
-  this.playerLasers,
-  this.enemies,
-  function shootEnemy(playerLaser, enemy) {
-    if (enemy) {
-      if (enemy.shot !== undefined) {
-        enemy.shot();
+  update() {
+    if (!this.player.getData('dead')) {
+      this.player.update();
+      if (this.keyW.isDown) {
+        this.player.up();
+      } else if (this.keyS.isDown) {
+        this.player.down();
       }
-      this.scoreTotal += 10;
-      this.scoreText.setText(`Score: ${this.scoreTotal}`);
-      enemy.dead(true);
-      playerLaser.destroy();
+      if (this.keyA.isDown) {
+        this.player.left();
+      } else if (this.keyD.isDown) {
+        this.player.right();
+      }
+      if (this.space.isDown) {
+        this.player.setData('shooting', true);
+      } else {
+        this.player.setData(
+          'shootTime',
+          this.player.getData('shotFrequency') - 1,
+        );
+        this.player.setData('shooting', false);
+      }
     }
-  },
-  null,
-  this,
-);
 
-// If shot then player is dead
-this.physics.add.overlap(
-  this.player,
-  this.enemyLasers,
-  function playerShot(player, laser) {
-    if (!player.getData('dead') && !laser.getData('dead')) {
-      this.sfx.gameOver.play();
-      player.dead(false);
-      player.shot();
-      laser.destroy();
-      this.sys.game.globals.score = this.scoreTotal;
-      this.scoreTotal = 0;
-    }
-  },
-  null,
-  this,
-);
+    // If crash then game over
+    this.physics.add.overlap(
+      this.player,
+      this.enemies,
+      function crash(player, enemy) {
+        if (!player.getData('dead') && !enemy.getData('dead')) {
+          this.sfx.gameOver.play();
+          player.dead(false);
+          player.shot();
+          enemy.dead(true);
+          this.sys.game.globals.score = this.scoreTotal;
+          this.scoreTotal = 0;
+        }
+      },
+      null,
+      this,
+    );
 
-// DELETE enemies if they haven't been shot
-for (let i = 0; i < this.enemies.getChildren().length; i += 1) {
-  const enemy = this.enemies.getChildren()[i];
+    // If shot enemy then enemy dies
+    this.physics.add.collider(
+      this.playerLasers,
+      this.enemies,
+      function shootEnemy(playerLaser, enemy) {
+        if (enemy) {
+          if (enemy.shot !== undefined) {
+            enemy.shot();
+          }
+          this.scoreTotal += 10;
+          this.scoreText.setText(`Score: ${this.scoreTotal}`);
+          enemy.dead(true);
+          playerLaser.destroy();
+        }
+      },
+      null,
+      this,
+    );
 
-  enemy.update();
+    // If shot then player is dead
+    this.physics.add.overlap(
+      this.player,
+      this.enemyLasers,
+      function playerShot(player, laser) {
+        if (!player.getData('dead') && !laser.getData('dead')) {
+          this.sfx.gameOver.play();
+          player.dead(false);
+          player.shot();
+          laser.destroy();
+          this.sys.game.globals.score = this.scoreTotal;
+          this.scoreTotal = 0;
+        }
+      },
+      null,
+      this,
+    );
 
-  if (enemy.x < -enemy.displayWidth
+    // DELETE enemies if they haven't been shot
+    for (let i = 0; i < this.enemies.getChildren().length; i += 1) {
+      const enemy = this.enemies.getChildren()[i];
+
+      enemy.update();
+
+      if (enemy.x < -enemy.displayWidth
     || enemy.x > config.width + enemy.displayWidth
     || enemy.y < -enemy.displayHeight * 4
     || enemy.y > config.height + enemy.displayHeight) {
-    if (enemy) {
-      if (enemy.shot !== undefined) {
-        enemy.shot();
+        if (enemy) {
+          if (enemy.shot !== undefined) {
+            enemy.shot();
+          }
+          enemy.destroy();
+        }
       }
-      enemy.destroy();
     }
-  }
-}
 
-// DELETE enemy lasers
-for (let i = 0; i < this.enemyLasers.getChildren().length; i += 1) {
-  const laser = this.enemyLasers.getChildren()[i];
-  laser.update();
-  if (laser.x < -laser.displayWidth
+    // DELETE enemy lasers
+    for (let i = 0; i < this.enemyLasers.getChildren().length; i += 1) {
+      const laser = this.enemyLasers.getChildren()[i];
+      laser.update();
+      if (laser.x < -laser.displayWidth
     || laser.x > config.width + laser.displayWidth
     || laser.y < -laser.displayHeight * 4
     || laser.y > config.height + laser.displayHeight) {
-    if (laser) {
-      laser.destroy();
+        if (laser) {
+          laser.destroy();
+        }
+      }
+    }
+    // DELETE player lasers if haven't shot an enemy
+    for (let i = 0; i < this.playerLasers.getChildren().length; i += 1) {
+      const laser = this.playerLasers.getChildren()[i];
+      laser.update();
+      if (laser.x < -laser.displayWidth
+    || laser.x > config.width + laser.displayWidth
+    || laser.y < -laser.displayHeight * 4
+    || laser.y > config.height + laser.displayHeight) {
+        if (laser) {
+          laser.destroy();
+        }
+      }
     }
   }
 }
