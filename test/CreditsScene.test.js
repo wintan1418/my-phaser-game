51ea35183aaa724882deae0a2c0e549d074605e3
oluwadare <wintan1418@gmail.com>
2021-06-24 10:26:36 +0100

    fix failing test

--- test/CreditsScene.test.js
+++ test/CreditsScene.test.js
@@ -5,10 +5,10 @@ it('checks if the class CreditsScene is defined', () => {
   expect(CreditsScene).toBeDefined();
 });
 
-it('CreditsScene is a subclass of Phaser.Scene', () => {
+it('CreditsScene is a working subclass of Phaser.Scene', () => {
   expect(CreditsScene).toBeSubclassOf(Phaser.Scene);
 });
 
-it('CreditsScene has a constructor', () => {
+it('CreditsScene has a functional constructor', () => {
   expect(CreditsScene.prototype.constructor).not.toBe(false);
 });
--- test/get-scores.test.js
+++ test/get-scores.test.js
@@ -2,7 +2,7 @@ import { url, getScores } from '../src/components/api';
 
 global.fetch = jest.fn(() => Promise.resolve({
   ok: true,
-  json: () => Promise.resolve({ result: [{ user: 'Daniel', score: 100 }] }),
+  json: () => Promise.resolve({ result: [{ user: 'wintan', score: 100 }] }),
 }));
 
 beforeEach(() => {
@@ -16,11 +16,11 @@ it('checks if returns correct url', () => {
 
 it('checks if wrong url is returned', () => {
   getScores(url);
-  expect(fetch).not.toHaveBeenCalledWith('www.youtube.com');
+  expect(fetch).not.toHaveBeenCalledWith('www.netlify.com');
 });
 
 it('retrieves data using the api and checks if it matches', () => {
   getScores(url).then((data) => {
-    expect(data).toEqual([{ user: 'Daniel', score: 100 }]);
+    expect(data).toEqual([{ user: 'wintan', score: 100 }]);
   });
 });
