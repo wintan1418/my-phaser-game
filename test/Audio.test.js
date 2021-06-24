c32e984a2eb5f28414087442d869599c1821dedc
oluwadare <wintan1418@gmail.com>
2021-06-24 10:21:38 +0100

    add test for audio

--- test/Audio.test.js
+++ test/Audio.test.js
@@ -4,21 +4,21 @@ import Audio from '../src/util/Audio';
 
 const audio = new Audio();
 
-it('checks if audio turns on', () => {
+it('This checks if audio turns on', () => {
   audio.musicOn = true;
   expect(audio.musicOn).toBeTruthy;
 });
 
-it('checks if audio turns off', () => {
+it('This checks if audio turns off', () => {
   audio.musicOn = false;
   expect(audio.musicOn).toBeFalsy;
 });
 
-it('checks if the class Audio is defined', () => {
+it('This checks if the class Audio is actually defined', () => {
   expect(Audio).toBeDefined();
 });
 
-it('Audio has a constructor', () => {
+it('This checks if Audio has a constructor', () => {
   expect(Audio.prototype.constructor).not.toBe(false);
 });
 
